0fb6fdc0d124e177d9949d582a27399ad78f0cb5
Tommy Cox <55894704+TommyCox@users.noreply.github.com>
2020-04-18 21:48:58 -0600

    create main.

---
+++ main.py
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------- #
+# Libraries
+# ---------------------------------------------------------------------------- #
+from pathlib import Path
+
+# ---------------------------------------------------------------------------- #
+# Local Modules
+# ---------------------------------------------------------------------------- #
+from server_controller import BDS_Wrapper as StartServer
+from wrapper_gui import GUI as MakeGUI
+
+# ---------------------------------------------------------------------------- #
+# Actual Program
+# ---------------------------------------------------------------------------- #
+server_path = Path("bedrock_server.exe")
+
+gui = MakeGUI()
+
+server = StartServer(server_path)
+console = server.read_output(output_handler = gui.write_console)
+console.start()
+
+gui.bind_inputs(server.write)
+
+gui.mainloop()
+
+if server.is_running():
+	server.write("stop")
+
+console.join()
